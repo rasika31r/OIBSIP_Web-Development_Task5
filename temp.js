309b297825218e066decb1e29f79a070d1409a54
rasika31r <rasikabirajdar2@gmail.com>
2025-08-02 19:42:18 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,31 +1,3 @@
-/* function convertTemp() {
-  const degrees = parseFloat(document.getElementById('degrees').value);
-  const type = document.getElementById('type').value;
-  const resultField = document.getElementById('result');
-
-  if (isNaN(degrees)) {
-    resultField.value = "Invalid input";
-    return;
-  }
-
-  let result = "";
-
-  switch (type) {
-    case "Celsius":
-      result = `${(degrees * 9 / 5 + 32).toFixed(4)} °F`;
-      break;
-    case "Fahrenheit":
-      result = `${((degrees - 32) * 5 / 9).toFixed(4)} °C`;
-      break;
-    case "Kelvin":
-      result = `${(degrees - 273.15).toFixed(4)} °C`;
-      break;
-  }
-
-  resultField.value = result;
-} */
-
-
 
   function convertTemp() {
   const degrees = parseFloat(document.getElementById('degrees').value);
@@ -61,4 +33,5 @@
   }
 
   resultField.value = result;
+
 }
